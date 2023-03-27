ac1f665fcbe179cc455f3068bd3a1b21a7778b69
marteenaF <martina.fatato@gmail.com>
2023-03-27 22:29:24 +0100

    (feat): Install, add and use vuetify

--- film_lottery/src/App.vue
+++ film_lottery/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App Shitty"/>
+  <TestComponent />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TestComponent from "./components/TestComponent.vue";
 
 export default {
 	name: "App",
 	components: {
-		HelloWorld
+		TestComponent,
 	}
 };
 </script>
---
+++ film_lottery/src/components/TestComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-btn>Click Me!</v-btn>
+</template>
+
+<script>
+export default{
+	name:"TestComponent",
+};
+</script>
