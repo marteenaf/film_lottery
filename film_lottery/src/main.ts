5000bb4386bc716b27bb3dbc87b9f3fa33e9978a
Martina <martina.fatato@gmail.com>
2023-04-01 23:42:58 +0100

    (feat): Add dummy data to work with

--- film_lottery/src/main.ts
+++ film_lottery/src/main.ts
@@ -6,7 +6,7 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { aliases, md } from "vuetify/iconsets/md";
-import "material-design-icons-iconfont/dist/material-design-icons.css"; 
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 const vuetify = createVuetify({
 	components,
@@ -18,9 +18,10 @@ const vuetify = createVuetify({
 			md,
 		},
 	},
-	theme:{
-		defaultTheme:"light",
+	theme: {
+		defaultTheme: "light",
 	}
 });
 
 createApp(App).use(vuetify).mount("#app");
+
