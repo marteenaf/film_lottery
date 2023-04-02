554d2f0d08f1e84d97284866197d38fbe48269b7
Martina <martina.fatato@gmail.com>
2023-04-02 23:37:38 +0100

    (feat): Create home button and add to components
    - Created some main pages to place the home button in
    - Added some re-directs for routes

---
+++ film_lottery/src/components/views/MenuView.vue
@@ -0,0 +1,16 @@
+<template>
+  <HomeButton routePath="/"></HomeButton>
+  <h1>Menu</h1>
+  <div class="menu-wrapper">
+    <v-btn to="/new-list">New List</v-btn>
+    <v-btn>Diary</v-btn>
+  </div>
+</template>
+<script>
+import HomeButton from "../reusable/HomeButton.vue";
+
+export default{
+	name: "MenuView",
+	components: { HomeButton }
+};
+</script>
