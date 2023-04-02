62a0d0c46dcee1a14a01f5516c409bd563a3cdd1
Martina <martina.fatato@gmail.com>
2023-04-02 19:07:17 +0100

    (feat): Install vue-router - add list view comp

---
+++ film_lottery/src/components/views/ListView.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ list.name || "No selected list" }}</h1>
+  <PickList />
+</template>
+<script>
+import { useListStore } from "@/stores/listsStore";
+export default {
+	name: "ListView",
+	components: {
+	},
+	data() {
+		return {
+			listState: useListStore(),
+			list: {}
+		};
+	},
+	async mounted() {
+		console.debug("[List View] Mounting...");
+		this.list = this.listState.selectedList;
+
+	}
+};
+</script>
+<style></style>
