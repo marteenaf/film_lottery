356fdb768d5d5bb287df6fcc0b89e028f29b0c7b
Martina <martina.fatato@gmail.com>
2023-04-02 22:22:03 +0100

    (feat): Add new list route

---
+++ film_lottery/src/components/views/NewListView.vue
@@ -0,0 +1,21 @@
+<template>
+<h1>New List {{ key }}</h1>
+</template>
+<script>
+export default{
+	name:"NewList",
+	data(){
+		return {
+			list:null,
+			name:"",
+			maxLength:5,
+			users:[],
+			movies:[],
+			key:"name"
+		};
+	},
+	mounted(){
+		this.key = this.$route.params.key;
+	}
+};
+</script>
