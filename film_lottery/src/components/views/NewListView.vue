150316031d7145b9bdf845f2cf7996b0d90c6693
Martina <martina.fatato@gmail.com>
2023-04-04 22:17:16 +0100

    (feat): Refine add users input

--- film_lottery/src/components/views/NewListView.vue
+++ film_lottery/src/components/views/NewListView.vue
@@ -8,40 +8,44 @@
 		<v-window-item value="maxLength">
 			<h4>Pick a size</h4>
 			<v-item-group v-model="maxLength" mandatory>
-				<v-item v-for="size in sizes" :key="size" :value="size">
+				<v-item v-for="(size, i) in sizes" :key="size" :value="size">
 					<template #default="{ isSelected, toggle }">
-						<v-btn variant="outlined" :size="size * 5" icon="" @click="toggle()" :class="isSelected ? 'filled' : ''">{{ size
+						<v-btn variant="outlined" :size="resized[i]" icon="" @click="toggle()" :class="isSelected ? 'filled' : ''">{{
+							size
 						}}</v-btn>
 					</template>
 				</v-item>
 			</v-item-group>
 		</v-window-item>
 		<v-window-item value="users">
-			<v-container>
-				<v-row justify="start">
-					<v-col v-for="(selection, i) in selections" :key="selection.text" cols="auto" class="py-1 pe-0">
-						<v-chip :disabled="loading" closable @click:close="users.splice(i, 1)">
-							<v-icon icon="person" start></v-icon>
-							{{ selection.userName }}
-						</v-chip>
-					</v-col>
-					<v-col v-if="!allSelected" cols="12">
-						<v-text-field variant="outlined" ref="search" v-model="search" hide-details label="Search" single-line></v-text-field>
-					</v-col>
-				</v-row>
-			</v-container>
-			<v-list>
+			<h4>Add other users</h4>
+			<div class="wrapper">
+				<v-chip v-for="(selection, i) in selections" :key="selection.email" closable @click:close="users.splice(i, 1)"
+					variant="outlined">
+					<v-icon icon="account_circle" start></v-icon>
+					{{ selection.userName }}
+				</v-chip>
+			</div>
+			<v-text-field v-if="!allSelected" variant="outlined" ref="search" v-model="search" hide-details
+				label="Search by email" single-line></v-text-field>
+			<v-list v-if="search !== ''" variant="text">
 				<template v-for="item in categories">
-					<v-list-item v-if="!users.includes(item)" :key="item.email" :disabled="loading" @click="users.push(item)">
-						<v-list-item-title v-text="item.userName"></v-list-item-title>
+					<v-list-item v-if="!users.includes(item)" :key="item.email" @click="users.push(item)">
+						<template #prepend>
+							<v-icon icon="person"></v-icon>
+						</template>
+						<v-list-item-title>{{ item.email }}</v-list-item-title>
+						<v-list-item-subtitle>{{ item.userName }}</v-list-item-subtitle>
 					</v-list-item>
 				</template>
 			</v-list>
 		</v-window-item>
 	</v-window>
-	<h4>{{ name }}</h4>
-	<h4>{{ maxLength }}</h4>
-	<h4 v-for="user in users" :key="user">{{ user }}</h4>
+	<div class="wrapper">
+		<h4>{{ name }}</h4>
+		<h4>{{ maxLength }}</h4>
+		<h4 v-for="user in users" :key="user">{{ user }}</h4>
+	</div>
 	<MenuButton icon="done" @click="nextStep"></MenuButton>
 </template>
 <script>
@@ -62,12 +66,12 @@ export default {
 			maxLength: 6,
 			users: [],
 			key: "name",
-			sizes: [6, 10, 20],
-			search:"",
-			items:[]
+			sizes: [1, 3, 6, 10, 20],
+			search: "",
+			items: []
 		};
 	},
-	beforeMount(){
+	beforeMount() {
 		this.userState.queryAllUsers();
 	},
 	mounted() {
@@ -79,7 +83,7 @@ export default {
 			this.key = "name";
 		}
 		this.items = this.userState.allUsers;
-		console.debug("All users",this.userState.allUsers);
+		console.debug("All users", this.userState.allUsers);
 	},
 	methods: {
 		prevStep() {
@@ -113,35 +117,48 @@ export default {
 		console.debug("[New List] Updating...", this.key);
 		//this.key = this.$route.params.key;
 	},
-	computed:{
-		allSelected () {
+	computed: {
+		allSelected() {
 			return this.users.length === this.items.length;
 		},
-		categories () {
+		categories() {
 			const search = this.search.toLowerCase();
-
 			if (!search) return this.items;
-
 			return this.items.filter(item => {
-				const text = item.text.toLowerCase();
-
+				const text = item.email.toLowerCase().split("@")[0];
+				//better search in order
 				return text.indexOf(search) > -1;
 			});
 		},
-		selections () {
+		selections() {
 			const selections = [];
-
 			for (const selection of this.users) {
 				selections.push(selection);
 			}
-
 			return selections;
 		},
+		resized() {
+			const mapRange = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+			const min = Math.min(...this.sizes);
+			const max = Math.max(...this.sizes);
+
+			return this.sizes.map(s => {
+				return mapRange(s, min, max, 30, 100);
+			});
+		}
 	}
 };
 </script>
 <style scoped>
 .filled {
 	background: #3761F9;
+}
+
+.wrapper {
+	margin: 10px 0px;
+}
+
+.wrapper-h {
+	margin: 0px 10px;
 }
 </style>
