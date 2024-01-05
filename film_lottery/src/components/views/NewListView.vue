e183f58f2358de55ecc60b3fddec5c079ef24f1b
Martina <martina.fatato@gmail.com>
2024-01-05 16:47:55 +0000

    (feat): Make new list an overlay

--- film_lottery/src/components/views/NewListView.vue
+++ film_lottery/src/components/views/NewListView.vue
@@ -1,34 +1,38 @@
 <template>
-  <v-form :title="'New List'" @submit.prevent="validate">
-    <MainLayout>
-      <template #content>
-        <v-col>
-          <h1>New List</h1>
-          <v-text-field :label="'Name'" v-model="form.name" :rules="rules"></v-text-field>
-          <v-slider :min="1" :max="10" :step="1" show-ticks thumb-label :label="'Movies per user'"
-            v-model="form.length"></v-slider>
-          <v-combobox :items="[]" :item-title="'email'" multiple chips clearable v-model="form.users" item-value="email"
-            :rules="rules" label="Users"></v-combobox>
-        </v-col>
-      </template>
-      <template #fab>
-        <v-col>
-          <v-btn type="submit" color="primary" variant="elevated">Create List</v-btn>
-          <v-btn @click="this.$router.push({ name: 'Home' })" class="text-decoration-underline" color="primary"
-            variant="text">Back to Lists</v-btn>
-        </v-col>
-      </template>
-    </MainLayout>
-  </v-form>
+  <OverlayLayout ref="overlay">
+    <v-form :title="'New List'" @submit.prevent="validate">
+      <MainLayout>
+        <template #content>
+          <v-col>
+            <h1>New List</h1>
+            <v-text-field :label="'Name'" v-model="form.name" :rules="rules"></v-text-field>
+            <v-slider :min="1" :max="10" :step="1" show-ticks thumb-label :label="'Movies per user'"
+              v-model="form.length"></v-slider>
+            <v-combobox :items="[]" :item-title="'email'" multiple chips clearable v-model="form.users" item-value="email"
+              :rules="rules" label="Users"></v-combobox>
+          </v-col>
+        </template>
+        <template #fab>
+          <v-col>
+            <v-btn type="submit" color="primary" variant="elevated">Create List</v-btn>
+            <v-btn @click="this.$router.push({ name: 'Home' })" class="text-decoration-underline" color="primary"
+              variant="text">Back to Lists</v-btn>
+          </v-col>
+        </template>
+      </MainLayout>
+    </v-form>
+  </OverlayLayout>
 </template>
 <script>
 import MainLayout from "@/components/layouts/MainLayout.vue";
+import OverlayLayout from "@/components/layouts/OverlayLayout.vue";
 import { useListStore } from "@/stores/listsStore";
 import { useUserStore } from "@/stores/usersStore";
 export default {
   name: "NewListView",
   components: {
-    MainLayout
+    MainLayout,
+    OverlayLayout
   },
   data() {
     return {
@@ -48,6 +52,7 @@ export default {
   },
   mounted() {
     console.debug("[New List View] Mounting...");
+    this.$refs.overlay.open();
   },
   methods: {
     async createNewList() {
