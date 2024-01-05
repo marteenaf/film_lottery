4bba58d748e6f2eb64762fd6a1314f005a4c96e3
Martina <martina.fatato@gmail.com>
2024-01-05 16:48:37 +0000

    (feat): Implement max count for picking

--- film_lottery/src/components/views/PickMovieView.vue
+++ film_lottery/src/components/views/PickMovieView.vue
@@ -2,7 +2,8 @@
   <v-container class="" style="height:100%">
     <v-row class="">
       <v-col style="height:200px;" class="d-flex justify-center align-center">
-        <h1 class="text-center">{{ selectedMovie ? selectedMovie.title : "No movie" }}</h1>
+        <h1 v-if="count != max" class="text-center">{{ selectedMovie ? selectedMovie.title : "No movie" }}</h1>
+        <h1 v-else class="text-center">No more pick attempts for now!</h1>
       </v-col>
     </v-row>
     <v-row class="">
@@ -12,14 +13,19 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col align="center" :cols="cols">
+      <v-col align="center" :cols="cols" v-if="disabled">
         <PickButton :text="'Back to List'" :color="'lightgrey'" :shadowColor="'grey'" @pick="toList">
         </PickButton>
       </v-col>
-      <v-col align="center" :cols="cols">
+      <v-col align="center" :cols="cols" v-else>
         <PickButton :text="'Watch Movie'" :color="'green'" :shadowColor="'darkgreen'" @pick="watchMovie"
           :disabled="disabled">
         </PickButton>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col style="height:200px;" class="d-flex justify-center align-center">
+        <h1>{{ max - count }}</h1>
       </v-col>
     </v-row>
   </v-container>
@@ -37,7 +43,7 @@ export default {
   data() {
     return {
       count: 0,
-      max: this.maxPicks ? this.maxPicks : 10,
+      max: this.maxPicks ? this.maxPicks : 3,
       store: useListStore(),
       selectedMovie: null,
     };
